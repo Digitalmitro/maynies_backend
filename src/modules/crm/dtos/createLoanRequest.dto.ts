ca76f6909a7549a061a163314b06e1e5856ac54e
soumendigitalmitro <soumen.digitalmitro@gmail.com>
2025-07-28 17:48:48 +0530

    load dto changes

--- src/modules/crm/dtos/createLoanRequest.dto.ts
+++ src/modules/crm/dtos/createLoanRequest.dto.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const createLoanRequestSchema = z.object({
 
-    amount: z.number({
+    amount: z.string({
         required_error: "Amount is required",
-        invalid_type_error: "Amount must be a number",
+        invalid_type_error: "Amount must be a string",
     }).min(1000, "Amount should be at least ₹1000"),
 
-    durationMonths: z.number({
+    durationMonths: z.string({
         required_error: "Duration is required",
-        invalid_type_error: "Duration must be a number",
+        invalid_type_error: "Duration must be a string",
     }).min(1, "Duration must be at least 1 month"),
 
     reason: z.string().min(10, "Reason must be at least 10 characters long").optional(),
